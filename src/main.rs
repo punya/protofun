392fc3ab2e6c765701de88dee03cbaae0f0726b0
Punya Biswal <punya@lumaril.com>
2025-12-17 10:56:54 -0500

    Builder-esque

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use protobuf::proto;
+use protobuf::prelude::*;
 
 include!(concat!(env!("OUT_DIR"), "/protobuf_generated/generated.rs"));
 
 fn main() {
-    let foo = proto!(Foo {
-        bar: Bar {
-            baz: 42,
-        },
-        maybe_bar: Bar {
-            baz: 33,
-        },
+    let foo_base = proto!(Foo {
+        bar: Bar { baz: 42 },
+        maybe_bar: Bar { baz: 33 },
     });
+
+    let mut foo_builder = foo_base;
+    foo_builder.bar_mut().set_baz(16);
+
+    let foo = foo_builder;
 
     println!("{foo:?}");
 }
